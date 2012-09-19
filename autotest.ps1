1bff11741b90ba72f262adc1647a2f39c95e8eab
Snahider <snahider@gmail.com>
2012-09-19 01:01:00 -0500

    Auto Install PsWatch

--- autotest.ps1
+++ autotest.ps1
@@ -1,12 +1,10 @@
 
 function InstallPsWatch(){
-	<#
 	$m = Get-Module -List pswatch 
 	if(!$m) {
 		Write-Host "You don't have the pswatch cmdlet installed. Let me handle that for you."
 		iex ((new-object net.webclient).DownloadString("http://bit.ly/Install-PsWatch"))
 	}
-	#>
 }
 
 function RunTests()
